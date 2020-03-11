c5acf1fb23ee1bbef443ea3e7edd31f69a1f69fb
Yash Jain <yash0307jain@gmail.com>
2020-03-11 22:34:24 +0530

    basic setup of user joining and disconnection is complete

--- client/src/components/Chat/Chat.component.jsx
+++ client/src/components/Chat/Chat.component.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 
 import "./Chat.styles.css";
 
+let socket;
+
 const Chat = () => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const ENDPOINT_URL = "localhost:5000";
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(window.location.search);
+
+    socket = io(ENDPOINT_URL);
+
+    setName(name);
+    setRoom(room);
+
+    socket.emit("join", { name, room }, () => {});
+
+    return () => {
+      socket.emit("disconnect");
+      socket.off();
+    };
+  }, [ENDPOINT_URL, window.location.search]);
+
   return <h1>Chat</h1>;
 };
 
