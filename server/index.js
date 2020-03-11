98f9d990b40e3dfdb19843bbda20f9ac84789adc
Yash Jain <yash0307jain@gmail.com>
2020-03-11 20:14:30 +0530

    setting up the project with all the files

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(require("./router"));
+
+server.listen(PORT, () => {
+  console.log(`Server has started on port ${PORT}`);
+});
